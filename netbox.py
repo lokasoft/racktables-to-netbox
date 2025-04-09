9f605c85c08497f09f08c9344cb5d24b4d4f77b2
Daniel Hansson <mailto@danielhansson.nu>
2025-04-09 14:39:47 +0200

    Delete netbox.py

--- netbox.py
+++
@@ -1,5 +0,0 @@
-"""
-Bridge module that imports NetBox from custom_netbox
-This allows code that imports from 'netbox' to work with our custom wrapper
-"""
-from custom_netbox import ExtendedNetBox as NetBox
